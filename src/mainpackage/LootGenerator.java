08428f8a7ea90d0682f31897ad9b4bbefc22fdba
alifaiza <alifaiza@grinnell.edu>
2016-11-14 20:57:17 -0600

    Update LootGenerator.java

--- src/mainpackage/LootGenerator.java
+++ src/mainpackage/LootGenerator.java
@@ -9,10 +9,18 @@ public class LootGenerator {
 	public Parser parser;
 	Random rand = new Random();
 
+	/**
+	 * Constructor for a new LootGenerator
+	 */
 	public LootGenerator() {
 		parser = new Parser();
 	}
 
+	/**
+	 * Picks a random monster from the monster list
+	 * @param monsterList a list of the monsters parsed from the monster file
+	 * @return Monster a Monster object
+	 */
 	public Monster pickMonster(ArrayList<Monster> monsterList) {
 		int index = rand.nextInt(monsterList.size());
 		Monster randomMonster = monsterList.get(index);
@@ -20,10 +28,22 @@ public class LootGenerator {
 
 	}
 
+	/**
+	 * Returns the treasure class of a monster
+	 * @param monster a Monster object
+	 * @return String representing the treasure class
+	 */
 	public String fetchTreasureClass(Monster monster)  {
 		return monster.getTreasure();
 	}
 
+	/**
+	 * Helper for the generateBaseItem method
+	 * @param chosenItem a base item we are searching for
+	 * @param itemsList array list containing the three possible item drops for a treasure class
+	 * @param treasureMap a map of a treasure class and its three possible item drops
+	 * @return String representing the base item dropped
+	 */
 	private String generateBaseItemHelper(String chosenItem, ArrayList<String> itemsList, 
 			Map<String, ArrayList<String>> treasureMap)  {
 		if (treasureMap.get(chosenItem) != null) {
@@ -36,6 +56,12 @@ public class LootGenerator {
 		}
 	}
 
+	/**
+	 * generates the base item that a monster drops
+	 * @param monster a Monster object
+	 * @param treasureMap a map of a treasure class and its three possible item drops
+	 * @return String representing the base item dropped
+	 */
 	public String generateBaseItem(Monster monster, Map<String, ArrayList<String>> treasureMap) {
 		ArrayList<String> itemsList = treasureMap.get(fetchTreasureClass(monster));
 		String chosenItem = itemsList.get(rand.nextInt(3));
@@ -44,7 +70,14 @@ public class LootGenerator {
 
 	}
 
-	//http://stackoverflow.com/questions/363681/generating-random-integers-in-a-specific-range
+	
+	/**
+	 * computes the base stats for a given base item
+	 * @param baseItem String containing the base item we are computing stats for
+	 * @param baseItemList an array list of base items and their respective defensive stats
+	 * @return String representing the stats for a given base item
+	 * @cite http://stackoverflow.com/questions/363681/generating-random-integers-in-a-specific-range
+	 */
 	public String generateBaseStats(String baseItem, ArrayList<BaseItem> baseItemList) {
 		for(BaseItem item : baseItemList)  {
 			if(baseItem.equals(item.getBaseItem()))  {
@@ -57,6 +90,12 @@ public class LootGenerator {
 		return "";
 	}
 
+	/**
+	 * generates a prefix and suffix for our item
+	 * @param prefixList a List containing all the prefixes and their respective additional stats
+	 * @param suffixList a List containing all the suffixes and their respective additional stats
+	 * @return Affix object
+	 */
 	public Affix generateAffix(ArrayList<Prefix> prefixList, ArrayList<Suffix> suffixList) {
 		Prefix prefix = null;
 		Suffix suffix = null;
